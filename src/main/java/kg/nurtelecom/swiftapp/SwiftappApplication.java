aa8e8f273d0281d07c7cdc14b4d9e5c5e1ac899b
kimgalina <genrietta.kim@gmail.com>
2024-03-03 22:15:16 +0600

    Initial project

---
+++ src/main/java/kg/nurtelecom/swiftapp/SwiftappApplication.java
@@ -0,0 +1,15 @@
+package kg.nurtelecom.swiftapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftappApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SwiftappApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/kg/nurtelecom/swiftapp/SwiftappApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.nurtelecom.swiftapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
